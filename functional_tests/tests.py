570274cc382c22cac2b35d3d4e5cd9335d6bb27d
nick5a1 <nickkarrasch@gmail.com>
2017-06-16 11:30:04 -0400

    new lists page

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -21,7 +21,7 @@ LOGGING = {
 }
 
 logging.config.dictConfig(LOGGING)
-logging.info('Hello')
+logging.info('Logging to console enabled')
 
 MAX_WAIT = 10
 
@@ -50,20 +50,20 @@ class NewVisitorTest(LiveServerTestCase):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.browser.get(self.live_server_url)
-
+    
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)  
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
-
+    
         # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
-
+    
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
         inputbox.send_keys('Buy peacock feathers')
-
+    
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)        
@@ -74,18 +74,11 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-
+    
         # The page updates again, and now shows both items on her list
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
-        
-        # Edith wonders whether the site will remember her list. Then she sees
-        # that the site has generated a unique URL for her -- there is some
-        # explanatory text to that effect.
-        #self.fail('Finish the test!')
-        
-        # She visits that URL - her to-do list is still there.
-
+    
         # Satisfied, she goes back to sleep
         
     def test_multiple_users_can_start_lists_at_different_urls(self):
